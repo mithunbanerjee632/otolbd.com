d02ee5736742cc940883cbd1c7caced22973e5d9
Mithun Banerjee <mithunbanerjee632@gmail.com>
2025-02-19 02:18:01 +0600

    Project setup

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
+
+
+
+Auth::routes();
+
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
