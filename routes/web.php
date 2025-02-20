5d026e5ca9c7a7b76fc5f93962359ef4c5a42f03
Mithun Banerjee <mithunbanerjee632@gmail.com>
2025-02-21 02:18:06 +0600

    Admin and User Dashboard

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdminController;
+use App\Http\Middleware\AuthAdmin;
 use Illuminate\Support\Facades\Auth;
 
 
@@ -9,3 +12,12 @@ use Illuminate\Support\Facades\Auth;
 Auth::routes();
 
 Route::get('/', [HomeController::class, 'index'])->name('home.index');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/user-dashboard', [UserController::class, 'index'])->name('user.index'); 
+});
+
+Route::middleware(['auth',AuthAdmin::class])->group(function () {
+    Route::get('/admin-dashboard', [AdminController::class, 'index'])->name('admin.index'); 
+});
+
